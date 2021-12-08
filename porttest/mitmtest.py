d8e3e93d8c085e300db644931d2f66dab61c2332
boboji5 <1248690876@qq.com>
2021-12-08 23:12:58 +0800

    采用mitmproxy来实现访问百度后输出hello world

--- porttest/mitmtest.py
+++ porttest/mitmtest.py
@@ -1,9 +1,15 @@
 from mitmproxy import http
 
 def request(flow: http.HTTPFlow):
+    with open("C:/Users/MIANENE/Desktop/test.json") as f:
+        if flow.request.pretty_url == "https://www.baidu.com/":
+            flow.response = http.HTTPResponse.mark(
+                200,
+                # b"Hello world!!!",
+                f.read(),    # 读取本地文件
+                {"Content-Type": "text/html"}
+            )
+
+def response(flow: http.HTTPFlow):
     if flow.request.pretty_url == "https://www.baidu.com/":
-        flow.response = http.HTTPResponse.mark(
-            200,
-            b"Hello world!!!",
-            {"Content-Type": "text/html"}
-        )
+        flow.response.text = "xxxxxx"
